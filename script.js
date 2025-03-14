15a5e600a302c049febd4a930100d79b32e50d36
Sky <skyengineer001@gmail.com>
2025-03-14 12:44:11 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const jonasAge = 31;
+const annaAge = 28;
+const lucasAge = 16;
+const ageForLicense = 18;
+
+if (jonasAge >= ageForLicense) {
+  console.log('Jonas is able to get car license');
+} else {
+  console.log('Jonas is not able to get car license');
+}
+
+if (annaAge >= ageForLicense) {
+  console.log('Anna is able to get car license');
+} else {
+  console.log('Anna is not able to get car license');
+}
+
+if (lucasAge >= ageForLicense) {
+  console.log('Lucas is able to get car license');
+} else {
+  console.log('Lucas is not able to get car license');
+}
